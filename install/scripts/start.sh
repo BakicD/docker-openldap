f92a313c0404de5f22652273fae130fa5bba1f23
David <david.bakic@outlook.com>
2017-05-23 16:53:12 +0200

    file updates and pyunit.py in works

--- install/scripts/start.sh
+++ install/scripts/start.sh
@@ -5,4 +5,9 @@
 cp /etc/hosts /tmp/hosts
 sed -e "s/$HOSTNAME\$/$HOSTNAME $SLAPDHOST/" /tmp/hosts > /etc/hosts
 
-su - ldap -c "slapd -4 -h ldap://$SLAPDHOST:$SLAPDPORT/ -d conns,config,stats,shell,trace -f /etc/openldap/slapd.conf"
+# start in background
+su - ldap -c "slapd -4 -h ldap://$SLAPDHOST:$SLAPDPORT/ -f /etc/openldap/slapd.conf"
+
+cp -Rp /var/db /var/db2
+# start in foreground
+su - ldap -c "slapd -4 -h ldap://$SLAPDHOST:9389/ -d conns,config,stats,shell,trace -f /etc/openldap/slapd2.conf"
