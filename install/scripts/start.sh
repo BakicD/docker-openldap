19cc8c9c0adb61a20ac8233f932bb9e7e8539b3d
David <david.bakic@outlook.com>
2017-05-23 17:11:58 +0200

    Revert "file updates and pyunit.py in works"
    This reverts commit f92a313c0404de5f22652273fae130fa5bba1f23.

--- install/scripts/start.sh
+++ install/scripts/start.sh
@@ -5,9 +5,4 @@
 cp /etc/hosts /tmp/hosts
 sed -e "s/$HOSTNAME\$/$HOSTNAME $SLAPDHOST/" /tmp/hosts > /etc/hosts
 
-# start in background
-su - ldap -c "slapd -4 -h ldap://$SLAPDHOST:$SLAPDPORT/ -f /etc/openldap/slapd.conf"
-
-cp -Rp /var/db /var/db2
-# start in foreground
-su - ldap -c "slapd -4 -h ldap://$SLAPDHOST:9389/ -d conns,config,stats,shell,trace -f /etc/openldap/slapd2.conf"
+su - ldap -c "slapd -4 -h ldap://$SLAPDHOST:$SLAPDPORT/ -d conns,config,stats,shell,trace -f /etc/openldap/slapd.conf"
